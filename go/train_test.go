74df88308f66bafd82f7d1d6c5f7d58824a7c5db
Nikki Nikkhoui <nnikkhoui@wikimedia.org>
2021-08-25 16:34:41 -0400

    initial go directory commit

---
+++ go/train_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"testing"
+)
+
+func testTrainOutput(t *testing.T) {
+	var train Train
+	// train := &Train{""}
+
+	// fmt.Println(train)
+
+}
+
+// func TestAbs(t *testing.T) {
+// 	got := Abs(-1)
+// 	if got != 1 {
+// 		t.Errorf("Abs(-1) = %d; want 1", got)
+// 	}
+// }
